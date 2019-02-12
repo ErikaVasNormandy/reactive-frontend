8d25ea82a7a4e7ae4466ade51684effbd412c55b
Erika Hudiono <erikavasnormandy@gmail.com>
2019-02-12 00:04:26 -0800

    removed cached

--- src/components/tab_projects/ProjectsComponent.js
+++ src/components/tab_projects/ProjectsComponent.js
@@ -13,7 +13,8 @@ class ProjectsComponent extends Component {
       	<h2>Projects Component</h2>
       	
       	<p>Ideas with a 'tentative' plan of action. I'll try linking them to a Git repo with more information and use this page as a general information page. </p>
-      <ProjectContainerComponent/></div>
+      	<ProjectContainerComponent/>
+      </div>
     );
   }
 }
--- src/components/tab_worldbuilding/WorldBuildingComponent.js
+++ src/components/tab_worldbuilding/WorldBuildingComponent.js
@@ -2,14 +2,16 @@
 
 import React, { Component } from 'react';
 
-
 import { BrowserRouter, Route, Link } from "react-router-dom";
+
+import WorldBuildingContainerComponent from './WorldBuildingContainerComponent'
 
 class WorldBuildingComponent extends Component {
   render() {
     return (
       <div>
       	<h2>World Building</h2>
+      	<WorldBuildingContainerComponent/>
       </div>
     );
   }
