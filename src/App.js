b87ae5b94f49b7d8fc0ee4a6344e865153d98103
Erika Hudiono <erikavasnormandy@gmail.com>
2019-02-18 00:13:57 -0800

    finished Xander's profile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import NavBarComponent from './components/NavBar/NavBarComponent'
 
-import { BrowserRouter, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
