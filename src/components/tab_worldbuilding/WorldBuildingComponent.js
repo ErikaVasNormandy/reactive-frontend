f11bcd899b0d42b9e16f1798a8e96c851a87d351
Erika Hudiono <erikavasnormandy@gmail.com>
2019-02-18 20:55:11 -0800

    meta tags without react-meta tags

--- src/components/tab_worldbuilding/WorldBuildingComponent.js
+++ src/components/tab_worldbuilding/WorldBuildingComponent.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 import WorldBuildingContainerComponent from './WorldBuildingContainerComponent'
-
+import MetaTags from 'react-meta-tags';
 
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
@@ -10,6 +10,12 @@ class WorldBuildingComponent extends Component {
   render() {
     return (
       <div>
+       <MetaTags>
+            <title>Projects</title>
+            <meta name="description" content="Projects either ongoing, finished, or planned" />
+            <meta property="og:image" content="https://s3-us-west-1.amazonaws.com/scavengers-repo.com/tab-projects/WebPreviewProjects.png" />
+          </MetaTags>
+
       	<h2>World Building</h2>
       	<WorldBuildingContainerComponent/>
       </div>
