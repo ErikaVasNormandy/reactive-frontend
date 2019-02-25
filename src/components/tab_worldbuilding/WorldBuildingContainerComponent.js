bc6a0a2448d360993bf458b647f9c49f3fe61f69
Erika Hudiono <erikavasnormandy@gmail.com>
2019-02-24 18:15:12 -0800

    Added a description for worldbuilding and fixed the menuimg

--- src/components/tab_worldbuilding/WorldBuildingContainerComponent.js
+++ src/components/tab_worldbuilding/WorldBuildingContainerComponent.js
@@ -112,42 +112,89 @@ class WorldBuildingContainerComponent extends Component {
       {/* Ping the mlab server and get the contents back*/}
             { this.getWorldBuildings()}
 
-
             {this.state.showAdminPanel ? <WorldBuildingInputComponent getWorldBuildings={this.getWorldBuildings}/> : null }
  
-            <button className="blue-grey lighten-4 adminBtn material-icons waves-effect waves-light btn " onClick={this.toggleAdminItem} value="showAdmin">palette</button>
+            <button className="blue-grey lighten-4 adminBtn material-icons waves-effect waves-light btn" onClick={this.toggleAdminItem} value="showAdmin">palette</button>
 
+            <table>
 
-              <ul className = "worldbuildinglist">
-                <li>Intro</li>
-                <li>Setting</li>
-                <li>Atomcraft</li>
-                <li>Environment</li>
-                <li>Politics</li>
-                <li>Characters</li>
-                <li>Plot</li>
-                <li></li>
-            </ul>
+              <tr>
+              <td>
+                <ul className = "worldbuildinglist">
+                  <li><h6>&nbsp;Intro</h6></li>
+                  <li><h6>&nbsp;Setting</h6></li>
+                  <li><h6>&nbsp;Atomcraft</h6></li>
+                  <li><h6>&nbsp;Environment</h6></li>
+                  <li><h6>&nbsp;Politics</h6></li>
+                  <li><h6>&nbsp;Characters</h6></li>
+                  <li><h6>&nbsp;Plot</h6></li>
+                  <li><h6>&nbsp;</h6></li>
+                </ul>
+              </td>
+              &nbsp;
+              &nbsp;
+              &nbsp;
+              &nbsp;
+              &nbsp;
+              &nbsp;
+              &nbsp;
+              &nbsp;
+              <td></td>
 
+              <td>
+                <h4>How to Use This Page</h4>
+                <p>
+                  This page has some semblance of organization, but mainly 
+                  tidbits that go into more detail without making you read an essay.
+                </p>
+                <p> 
+                  All of these take place in a setting called <strong>Arda Hyperion</strong>, 
+                  a timeline where humanity has discovered <strong>Atomcraft</strong>, the
+                  ability to interface with the Operating System running the Universe.
+                </p>
+                <p>
+                  While this technically includes extraordinary abilities such as matter 
+                  manipulation, teleportation, and energy conduction, their true hallmark is
+                  their expertise in the creation and use of <strong>Extradimensional Material (EM)</strong>. 
+                </p>
+                <p>
+                  For better or worse, humans have discovered how to conduct what is essentially Magic, and
+                  apply it on an industrial scale to their technology, economies, and infrastructure. 
+                  But all resources are finite, and for every miracle there must be a consequence. 
+                  Ever the intrepid craftsmen, humanity's hunger for resources and energy, whether 
+                  Earthbound or Cosmic in nature, will have its price. 
+                </p>
+              </td>
+
+              </tr>
+            </table>
             {/* Render out the contents via a list */}
             <br/>
             <h4>Intro</h4>
             <div id = "Introduction">
 
-              <p>Arda Hyperion is tentative webseries set in an alternate timeline where humans have discovered Atomcraft:
-               limited read-write access to the Operating System running the Universe.
+              <p>Arda Hyperion is tentative webseries set in an alternate timeline where humans 
+              have discovered Atomcraft: limited read-write access to the Operating System running the Universe.
 
-              Those who practice Atomcraft are known as "Atomancers", and are known for their expertise in the forging and use of Extradimensional Matter (EM). 
+              Those who practice Atomcraft are known as "Atomancers", and are known for their expertise 
+              in the forging and use of Extradimensional Matter (EM). 
 
-              An Atomcrafted tool will be comprised of modular components, constantly shifting to match the whims of the user who forged them. A firearm 
-              made from Extradimensional Matter will cycle through different firing modes depending on the skill of the user that crafted those components.   </p>
+              An Atomcrafted tool will be comprised of modular components, constantly shifting to match the 
+              whims of the user who forged them. A firearm made from Extradimensional Matter will cycle 
+              through different firing modes depending on the skill of the user that crafted those components.   </p>
              
               <table className="image">
-                <tr><td> <img src="https://66.media.tumblr.com/b9031267e0839b004b03fbc1dfb5c887/tumblr_perfuuBA3h1qgojgxo2_540.gif"></img></td></tr>
-                <tr><td className="caption">Preview pic from the game Control (2019) WIP weapon test. This is an approximation of what a modern (3rd, 4th generation)
-                Atomancer can do with enough time and practice </td></tr>
+                <tr>
+                  <td> 
+                    <img src="https://66.media.tumblr.com/b9031267e0839b004b03fbc1dfb5c887/tumblr_perfuuBA3h1qgojgxo2_540.gif"></img>
+                  </td>
+                  <td className="caption">Preview pic from the game Control (2019) WIP weapon test. This is an approximation of what a modern (3rd, 4th generation)
+                  Atomancer can do with enough time and practice 
+                  </td>
+                </tr>
               </table>
-            <br/>
+           
+
               <p>
                 There have been 3 generations of Atomancers, with each subsequent generation's Atomcraft ability to bypass reality declining
                 over time. While crafting modular components made of Extradimensional Matter that are lighter and stronger than any other material on Earth
